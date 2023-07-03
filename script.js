e5f5ea5d522ecb4da283cc7af995a9eae713df49
Mika Ramparany <mika.ramparany@oclock.school>
2023-07-03 16:22:11 +0200

    J'ai rajouté une condition qui affiche pendant quelques seconde le nom 'terre' quand la vague atteint earth-system.
    utilisation de settimeout pour la durée d'affichage et de réinitialisation du nom 'terre'

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const sun = document.querySelector('.sun'); // Sélectionnez la classe CSS avec un point devant "sun"
-const waveContainer = document.querySelector('.system-container'); // Sélectionnez la classe CSS avec un point devant "system-container"
+const sun = document.querySelector('.sun');
+const waveContainer = document.querySelector('.system-container');
+let earthNameVisible = false; // Je crée une variable de contrôle pour vérifier si la Terre est déjà affichée
+let displayTimeout = null; // Je crée une variable pour stocker le délai d'affichage en cours
 
 sun.addEventListener('click', createWave);
 
 function createWave(event) {
-  const wave = document.createElement('div'); // Créez un nouvel élément de vague à chaque clic
-  wave.classList.add('wave'); // Ajoutez la classe CSS "wave" à l'élément de vague
+  const wave = document.createElement('div');
+  wave.classList.add('wave');
 
   const sunRect = sun.getBoundingClientRect();
   const sunCenterX = sunRect.left + sunRect.width / 2;
@@ -18,11 +20,36 @@ function createWave(event) {
   wave.style.left = `${sunCenterX}px`;
   wave.style.top = `${sunCenterY}px`;
 
-
   sun.appendChild(wave);
 
   wave.addEventListener('animationend', () => {
     sun.removeChild(wave);
-    wave.id = ""; // je dois rajouter ça pour éviter que la vague ne reste à la fin de la fonction 
   });
+
+  if (!earthNameVisible && !displayTimeout) { // Je dois vérifier si la Terre n'est pas déjà affichée et si aucun délai d'affichage n'est en cours
+    const displayDelay = 1100; // On peut régler le délai avant l'affichage du nom de la Terre en millisecondes
+
+    displayTimeout = setTimeout(() => {
+      const earthSystem = document.querySelector('.earth-system');
+      const earthSystemRect = earthSystem.getBoundingClientRect();
+      const earthSystemCenterX = earthSystemRect.left + earthSystemRect.width / 2;
+      const earthSystemTop = earthSystemRect.top;
+
+      const earthName = document.createElement('span');
+      earthName.classList.add('earth-name');
+      earthName.textContent = 'Terre';
+      earthName.style.left = `${earthSystemCenterX}px`;
+      earthName.style.top = `${earthSystemTop - 40}px`;
+
+      earthSystem.appendChild(earthName);
+      earthNameVisible = true; // Indique que la Terre est affichée
+
+      const displayDuration = 4000; // Durée d'affichage du nom de la Terre en millisecondes, modifiable également comme on veut
+      setTimeout(() => {
+        earthSystem.removeChild(earthName);
+        earthNameVisible = false; // indique que la Terre n'est plus affichée
+        displayTimeout = null; // réinitialiser le délai d'affichage, pcq sinon je ne pouvait plus refaire la manip. Il aurait fallu recharger la page..
+      }, displayDuration);
+    }, displayDelay);
+  }
 }
